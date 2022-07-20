31119e77e4b7349d72e9903370332fa2229212c8
Jaga <jagblst@gmail.com>
2022-07-20 12:34:24 +0300

    практика по методам массивов

--- practice.js
+++ practice.js
@@ -66,3 +66,43 @@ while ( i > 1 ) {
     i--;
     out.innerHTML += `${i} `;
 }
+
+
+// filter
+const filterThis = [2, 4, 3, 6, 8, 9];
+
+const filter = (arr, fn) => {
+    const filteredArr = [];
+    for (let elem of arr) {
+        if (fn(elem)) filteredArr.push(elem);
+    }
+    return filteredArr;
+};
+
+console.log(filter(filterThis, (elem) => elem % 3 == 0));
+
+// some
+const someOfThis = ['Java', 'JavaScript', 'Python'];
+
+const some = (arr, fn) => {
+    for (let elem of arr) {
+        if (fn(elem)) return true;
+    }
+    return false;
+};
+
+console.log(some(someOfThis, (elem) => elem === "JavaS"));
+
+
+// map
+const mapMe = [1000, 800, 2400, 500];
+
+const map = (arr, fn) => {
+    const mappedArr = [];
+    for (let elem of arr) {
+        mappedArr.push(fn(elem));
+    }  
+    return mappedArr;
+};
+
+console.log(map(mapMe, (elem) => elem + 200));
