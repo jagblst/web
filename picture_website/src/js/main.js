ade90a7c633ef67a77900f9cd00acc2a2dc34c3c
Jaga <jagblst@gmail.com>
2022-09-15 15:43:45 +0300

    ShowMoreStyles
    Простая "подгрузка" элементов
    (добавить больше стилей)

--- picture_website/src/js/main.js
+++ picture_website/src/js/main.js
@@ -3,6 +3,7 @@ import sliders from "./modules/sliders";
 import forms from "./modules/forms";
 import mask from "./modules/mask";
 import checkNumImputs from "./modules/checkTextInputs";
+import showMorestyles from "./modules/showMoreStyles";
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
@@ -14,4 +15,5 @@ window.addEventListener('DOMContentLoaded', () => {
     mask('[name="phone"]');
     checkNumImputs('[name="name"]');
     checkNumImputs('[name="message"]');
+    showMorestyles('.button-styles', '.styles-2');
 });
