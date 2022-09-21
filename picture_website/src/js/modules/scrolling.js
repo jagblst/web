14dc39e563c1d8bc21247a92deeeb1cdabf5adf8
Jaga <jagblst@gmail.com>
2022-09-21 17:09:09 +0300

    Создаем плавный скролл на сайте
     requestAnimationFrame

--- picture_website/src/js/modules/scrolling.js
+++ picture_website/src/js/modules/scrolling.js
@@ -11,6 +11,41 @@ const scrolling = (upSelector) => {
         }
     });
 
+    // Scrolling with raf
+
+    let links = document.querySelectorAll('[href^="#"]'),
+        speed = 0.2;
+    
+    links.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+
+            let widthTop = document.documentElement.scrollTop,
+                hash = this.hash,
+                toBlock = document.querySelector(hash).getBoundingClientRect().top,
+                start = null;
+
+            requestAnimationFrame(step);
+            
+            function step (time) {
+                if (start === null) {
+                    start = time;
+                }
+
+                let progress = time - start,
+                    r = (toBlock < 0 ? Math.max(widthTop - progress/speed, widthTop + toBlock) :
+                    Math.min(widthTop + progress/speed, widthTop + toBlock));
+
+                    document.documentElement.scrollTo(0, r);
+
+                    if (r != widthTop + toBlock) {
+                        requestAnimationFrame(step);
+                    } else {
+                        location.hash = hash;
+                    }
+            }
+        });
+    });
 
     // Pure js scrolling
     // const element = document.documentElement,
