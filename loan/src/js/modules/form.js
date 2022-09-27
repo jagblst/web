560909442fa88ecb3a974e7cace3f1bf0e3d6189
Jaga <jagblst@gmail.com>
2022-09-27 12:26:15 +0300

    Работаем с формами отправки данных

---
+++ loan/src/js/modules/form.js
@@ -0,0 +1,60 @@
+export default class Form {
+    constructor(forms) {
+        this.forms = document.querySelectorAll(forms);
+        this.inputs = document.querySelectorAll('input');
+        this.message = {
+            loading: 'Loading...',
+            success: 'Thank you! We will contact you soon',
+            failure: 'Something gone wrong...'
+        };
+        this.path = 'assets/question.php';
+    }
+
+    clearInputs() {
+        this.inputs.forEach(item => {
+            item.value = '';
+        });
+    }
+
+    async postData(url, data) {
+        let res = await fetch(url, {
+            method: "POST",
+            body: data
+        });
+        return await res.text();
+    }
+
+    init() {
+        this.forms.forEach(item => {
+            item.addEventListener('submit', (e) => {
+                e.preventDefault();
+
+                let statusMessage = document.createElement('div');
+                statusMessage.style.cssText = `
+                margin-top: 15px;
+                font-size: 18px;
+                color: grey
+                `;
+                item.parentNode.appendChild(statusMessage);
+                statusMessage.textContent = this.message.loading;
+
+                const formData = new FormData(item);
+
+                this.postData(this.path, formData)
+                    .then(res => {
+                        console.log(res);
+                        statusMessage.textContent = this.message.success;
+                    })
+                    .catch(() => {
+                        statusMessage.textContent = this.message.failure;
+                    })
+                    .finally(() => {
+                        this.clearInputs();
+                        setTimeout(() => {
+                            statusMessage.remove();
+                        }, 6000);
+                    });
+            });
+        });
+    }
+}
