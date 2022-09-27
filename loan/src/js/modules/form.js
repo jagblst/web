647d034fcc34ffb34fa8d1be44e7c0b9ff11a76b
Jaga <jagblst@gmail.com>
2022-09-27 12:41:54 +0300

    Работаем с формами отправки данных
    Создаем маску телефона и запрет на ввод кирилицы

--- loan/src/js/modules/form.js
+++ loan/src/js/modules/form.js
@@ -15,6 +15,66 @@ export default class Form {
             item.value = '';
         });
     }
+    
+    checkMailInputs() {
+        const mailInputs = document.querySelectorAll('[type="email"]');
+    
+       mailInputs.forEach(input => {
+            input.addEventListener('keypress', function(e) {
+                if (e.key.match(/[^a-z 0-9 @ \.]/ig)) {
+                    e.preventDefault();
+                }
+            });
+        });
+    }
+
+    initMask() {
+
+    let setCursorPosition = (pos, elem) => {
+        elem.focus();
+
+        if (elem.setSelectionRange) {
+            elem.setSelectionRange(pos, pos);
+        } else if (elem.createTextRange) {
+            let range = elem.createTextRange();
+
+            range.collapse(true);
+            range.moveEnd('character', pos);
+            range.moveStart('character', pos);
+            range.select();
+        }
+    };
+
+    function createMask(event) {
+        let matrix = '+1 (___) ___-____',
+            i = 0,
+            def = matrix.replace(/\D/g, ''),
+            val = this.value.replace(/\D/g, '');
+        if (def.length >= val.length) {
+            val = def;
+        }
+
+        this.value = matrix.replace(/./g, function(a) {
+            return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? '' : a;
+        });
+
+        if (event.type === 'blur') {
+            if (this.value.length == 2) {
+                this.value = '';
+            }
+        } else {
+            setCursorPosition(this.value.length, this);
+        }
+    }
+
+    let inputs = document.querySelectorAll('[name="phone"]');
+
+    inputs.forEach(input => {
+        input.addEventListener('input', createMask);
+        input.addEventListener('focus', createMask);
+        input.addEventListener('blur', createMask);
+    });
+    }
 
     async postData(url, data) {
         let res = await fetch(url, {
@@ -25,6 +85,9 @@ export default class Form {
     }
 
     init() {
+        this.checkMailInputs();
+        this.initMask();
+        
         this.forms.forEach(item => {
             item.addEventListener('submit', (e) => {
                 e.preventDefault();
