c7d78a21194891ff5c09dd2dc1be427bdf8bf74a
Jaga <jagblst@gmail.com>
2022-09-29 16:26:35 +0300

    Элементы и компоненты
    React

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,21 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
-
-const text = 'Hello World';
-//  const elem = <h2>Hello World!</h2>;
-// const elem = React.createElement('h2', {className: 'greetings'}, 'Hello World!');
-const elem = (
-  <div>
-    <h2 className='text'>TEXT: {text}</h2>
-    <input type="text" />
-    <label htmlFor=""></label>
-    <button tabIndex="0">Click</button>  
-  </div>
-);
+import { Header } from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-elem
+<App/>
 );
