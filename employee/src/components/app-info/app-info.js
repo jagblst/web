b8c9ee1b6344bf25c54a70a5ab92a55c844eabd5
Jaga <jagblst@gmail.com>
2022-09-30 17:05:24 +0300

    Employees project
    App-info, Search-panel, App-filter

---
+++ employee/src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+const AppInfo = () => {
+    return (
+        <div>
+            <h1>Облік співробітників компанії</h1>
+            <h2>Загальна кількість співробітників: </h2>
+            <h2>Премію отримають:</h2>
+        </div>
+    )
+}
+
+export default AppInfo;
