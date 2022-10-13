578eaad257673437c948f1d79517a1862cfdd8ae
Jaga <jagblst@gmail.com>
2022-10-13 16:11:05 +0300

    Общее к-во сотрудников + премия

--- employee/src/components/app/app.js
+++ employee/src/components/app/app.js
@@ -74,11 +74,12 @@ class App extends Component {
         console.log(`Rise this ${id}`)
     }
 
-
    render () {
+        const employees = this.state.data.length;
+        const increased = this.state.data.filter(item => item.increase).length;
         return (
             <div className="app">
-                <AppInfo/>
+                <AppInfo employees={employees} increased={increased}/>
 
                 <div className='search-panel'>
             <SearchPanel/>
