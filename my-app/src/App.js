801c9797533b560713f47d0607efd92617483283
Jaga <jagblst@gmail.com>
2022-10-16 16:40:10 +0300

    Стили в React. Styled Components
    Styled Components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,34 @@
 import {Component, Fragment} from 'react';
+import styled from 'styled-components'
 import './App.css';
+
+const EmpItem = styled.div`
+  padding: 20px;
+  margin-bottom: 15px;
+  border-radius: 5px;
+  box-shadow: 5px 5px 10px rgba(0,0,0, .2);
+  a {
+      display: block;
+      margin: 10px 0 10px 0;
+      color: ${props => props.active ? 'orange' : 'black'};
+  }
+  input {
+    display: block;
+    margin-top: 10px;
+  }
+  `;
+
+const HeaderStyled = styled.h2`
+  font-size: 22px;
+  `;  
+
+export const Button = styled.button`
+  display: block;
+  padding: 5px 15px;
+  background-color: gold;
+  border: 1px solid rgba(0,0,0, .2);
+  box-shadow: 5px 5px 10px rgba(0,0,0, .2);
+`;
 
 class WhoAmI extends Component {
   constructor(props) {
@@ -28,18 +57,18 @@ class WhoAmI extends Component {
     const {name, surname, link} = this.props;
     const {position, years} = this.state;
     return(
-      <Fragment>
-        <button onclick={this.nextYear}>+++</button>
-        <h1>My name is {name}, 
+      <EmpItem active>
+        <Button onClick={this.nextYear}>+++</Button>
+        <HeaderStyled>My name is {name}, 
             surname - {surname}, 
             age - {years}, 
-            position - {position}</h1>
+            position - {position}</HeaderStyled>
         <a href={link}>My profile</a>
         <form>
             <span>Введіть посаду</span>
             <input type="text" onChange={(e) => this.comitInputChanges(e, 'some color')} />
         </form>
-      </Fragment>
+      </EmpItem>
     )
   }
 }
@@ -83,17 +112,20 @@ function Btn() {
   return <button>{logged ? 'Enter' : text}</button>
 } 
 
+const Wrapper = styled.div`
+  width: 600px;
+  margin: 80px auto 0 auto;
+  `;
+
 function App() {
   return (
-    <div className="App">
+    <Wrapper>
       <WhoAmI name="John" surname="Smith" link="facebook.com"/>
       <WhoAmI name="Alex" surname="Shepard" link="twitter.com"/>
-     <StrictMode>
      <Header/> 
-     </StrictMode>
       <Field/>
       <Btn/>   
-    </div>
+    </Wrapper>
   );
 }
 
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { Header } from './App';
+import { Button } from './App';
+import styled from 'styled-components';
+
+const BigButton = styled(Button)`
+  margin: 0 auto;
+  width: 245px;
+  text-align: center;
+`;
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 <StrictMode>
   <App/>
+  <BigButton as="a">Send report</BigButton>
 </StrictMode>
 );
