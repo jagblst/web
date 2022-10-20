c138483a377b3916b6aabbe6dc83594701517396
Jaga <jagblst@gmail.com>
2022-10-20 15:14:28 +0300

    дозагрузка персонажей
    charEnded

--- marvel/src/components/charList/CharList.js
+++ marvel/src/components/charList/CharList.js
@@ -11,7 +11,8 @@ class CharList extends Component {
         loading: true,
         error: false,
         newItemLoading: false,
-        offset: 210
+        offset: 1551,
+        charEnded: false
     }
     
     marvelService = new MarvelService();
@@ -34,11 +35,17 @@ class CharList extends Component {
     }
 
     onCharListLoaded = (newCharList) => {
+        let ended = false;
+        if (newCharList.length < 9) {
+            ended = true
+        }
+
         this.setState(({offset, charList}) => ({
             charList: [...charList, ...newCharList],
             loading: false,
             newItemLoading: false,
-            offset: offset + 9
+            offset: offset + 9,
+            charEnded: ended
         }))
     }
 
@@ -76,7 +83,7 @@ class CharList extends Component {
 
     render() {
 
-        const {charList, loading, error, newItemLoading, offset} = this.state;
+        const {charList, loading, error, newItemLoading, offset, charEnded} = this.state;
         
         const items = this.renderItems(charList);
 
@@ -92,6 +99,7 @@ class CharList extends Component {
                 <button 
                         className="button button__main button__long"
                         disabled={newItemLoading}
+                        style={{'display': charEnded ? 'none' : 'block'}}
                         onClick={() => this.onRequest(offset)}
                         >
                     <div className="inner">load more</div>
