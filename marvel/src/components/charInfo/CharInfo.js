caa9cb4061928f0810b52cf0c2facfbee2b3e372
Jaga <jagblst@gmail.com>
2022-10-20 12:55:01 +0300

    Error Boundaries
    Предохранители

--- marvel/src/components/charInfo/CharInfo.js
+++ marvel/src/components/charInfo/CharInfo.js
@@ -38,6 +38,7 @@ class CharInfo extends Component {
             .getCharacter(charId)
             .then(this.onCharLoaded)
             .catch(this.onError);
+     
     }
 
     onCharLoaded = (char) => {
