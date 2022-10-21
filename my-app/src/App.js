460bf5329926e4fd54bcf9200a4de6bd83e5ada6
Jaga <jagblst@gmail.com>
2022-10-21 15:28:41 +0300

    props.children
    Вставка элементов через props.children

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -115,11 +115,23 @@ function Btn() {
 const Wrapper = styled.div`
   width: 600px;
   margin: 80px auto 0 auto;
-  `;
+`;
+
+const DynamicGreating = (props) => {
+  return (
+    <div className={'mb-3 p-3 border border-' + props.color}>
+        {props.children}
+    </div>
+  )
+}
 
 function App() {
   return (
     <Wrapper>
+      <DynamicGreating color={'primary'}>
+            <h2>This weel was hard</h2>
+            <h2>Hello World!</h2>
+      </DynamicGreating>
       <WhoAmI name="John" surname="Smith" link="facebook.com"/>
       <WhoAmI name="Alex" surname="Shepard" link="twitter.com"/>
      <Header/> 
