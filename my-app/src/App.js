26c2c5d2b0449754c996cb9e2426d1ecaf5e7fbb
Jaga <jagblst@gmail.com>
2022-10-27 13:28:30 +0300

    useRef
    отображение предидущего State

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,26 @@
-import {useRef} from 'react';
+import {useRef, useState, useEffect} from 'react';
 import {Container} from 'react-bootstrap';
 import './App.css';
 
 const Form = () => {
-    const myRef = useRef(null);
+    const [text, setText] = useState('');
 
-    const focusFirstTI = () => {
-        myRef.current.focus();
-    }
+    const myRef = useRef(1);
+
+    useEffect(() => {
+        myRef.current = text;
+    })
            
     return (
         <Container>
             <form className="w-50 border mt-5 p-3 m-auto"> 
                 <div className="mb-3">
                     <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
-                    <input ref={myRef} type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+                    <input onChange={(e) => setText(e.target.value)} type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-                    <textarea onClick={focusFirstTI} className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    <textarea value={myRef.current} className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
                 </div> 
             </form>
         </Container>
