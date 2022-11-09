dea2588c0b519b366da34496484f5cac56284677
Jaga <jagblst@gmail.com>
2022-11-09 15:09:55 +0200

    React Lazy
    All pages

--- marvel/src/components/app/App.js
+++ marvel/src/components/app/App.js
@@ -1,11 +1,13 @@
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import { lazy, Suspense } from 'react';
 
-import {MainPage, ComicsPage, SingleComicPage} from '../pages';
 import AppHeader from "../appHeader/AppHeader";
 import Spinner from '../spinner/Spinner';
 
 const Page404 = lazy(() => import('../pages/404'));
+const MainPage = lazy(() => import('../pages/MainPage'));
+const ComicsPage = lazy(() => import('../pages/ComicsPage'));
+const SingleComicPage = lazy(() => import('../pages/SingleComicPage'));
 
 const App = () => {
     
