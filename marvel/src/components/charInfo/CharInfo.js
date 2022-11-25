9dc5748d6a6c70d1e0cc284d69a210cb11b52c2d
Jaga <jagblst@gmail.com>
2022-11-25 15:48:12 +0200

    FSM, Finite-state machine
    Принцип конечного автомата

--- marvel/src/components/charInfo/CharInfo.js
+++ marvel/src/components/charInfo/CharInfo.js
@@ -12,7 +12,7 @@ const CharInfo = (props) => {
 
     const [char, setChar] = useState(null);
        
-   const {loading, error, getCharacter, clearError} = useMarvelService();
+   const {loading, error, getCharacter, clearError, process, setProcess} = useMarvelService();
 
     useEffect(() => {
         updateChar();
@@ -26,23 +26,35 @@ const CharInfo = (props) => {
         clearError();
         getCharacter(charId)
             .then(onCharLoaded)
+            .then(() => setProcess('confirmed'))
         }
 
     const onCharLoaded = (char) => {
         setChar(char);
     }
 
-    const skeleton = char || loading || error ? null : <Skeleton/>
-    const errorMessage = error ? <ErrorMessage/> : null;
-    const spinner = loading ? <Spinner/> : null;
-    const content = !(loading || error || !char) ? <View char={char}/> : null;
+    const setContent = (process, char) => {
+        switch (process) {
+            case 'waiting':
+                return <Skeleton/>;
+                break;
+            case 'loading':
+                return <Spinner/>;
+                break;
+            case 'confirmed':
+                return <View char={char}/>;
+                break;
+            case 'error':
+                return <ErrorMessage/>;
+                break;
+            default:
+                throw new Error('Unexpected process state');                 
+        }
+    }
 
     return (
         <div className="char__info">
-            {skeleton}
-            {errorMessage}
-            {spinner}
-            {content}
+           {setContent(process, char)}
         </div>
     )    
 }
