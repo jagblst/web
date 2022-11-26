f7f28574d1c44bed3707700981b59093283b151c
Jaga <jagblst@gmail.com>
2022-11-26 15:20:16 +0200

    SetContent.js

--- marvel/src/components/charInfo/CharInfo.js
+++ marvel/src/components/charInfo/CharInfo.js
@@ -2,9 +2,7 @@ import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import useMarvelService from '../../services/MarvelService';
-import Spinner from '../spinner/Spinner';
-import ErrorMessage from '../errorMessage/ErrorMessage';
-import Skeleton from '../skeleton/Skeleton';
+import setContent from '../../utils/setContent';
 
 import './charInfo.scss';
 
@@ -33,34 +31,15 @@ const CharInfo = (props) => {
         setChar(char);
     }
 
-    const setContent = (process, char) => {
-        switch (process) {
-            case 'waiting':
-                return <Skeleton/>;
-                break;
-            case 'loading':
-                return <Spinner/>;
-                break;
-            case 'confirmed':
-                return <View char={char}/>;
-                break;
-            case 'error':
-                return <ErrorMessage/>;
-                break;
-            default:
-                throw new Error('Unexpected process state');                 
-        }
-    }
-
     return (
         <div className="char__info">
-           {setContent(process, char)}
+           {setContent(process, View, char)}
         </div>
     )    
 }
 
-const View = ({char}) => {
-    const {name, description, thumbnail, homepage, wiki, comics} = char;
+const View = ({data}) => {
+    const {name, description, thumbnail, homepage, wiki, comics} = data;
 
     let imgStyle = {'objectFit' : 'cover'};
     if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
