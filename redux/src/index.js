a9d8bdfa2ae1550d9ddcedcd64f8e37a97760622
Jaga <jagblst@gmail.com>
2022-12-01 13:05:45 +0200

    Counter on Redux

--- redux/src/index.js
+++ redux/src/index.js
@@ -1,6 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createStore } from 'redux';
 
+const initialState = 0;
+
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INC':
+      return state + 1;
+    case 'DEC':
+      return state - 1;
+    case 'RND':
+      return state * action.payload;
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+const update = () => {
+  document.getElementById('counter').textContent = store.getState()
+};
+
+store.subscribe(update);
+
+document.getElementById('inc').addEventListener('click', () => {
+  store.dispatch({type: 'INC'})
+});
+
+document.getElementById('dec').addEventListener('click', () => {
+  store.dispatch({type: 'DEC'})
+});
+
+document.getElementById('rnd').addEventListener('click', () => {
+  const value = Math.floor(Math.random() * 10);
+  store.dispatch({type: 'RND', payload: value})
+});
+
+// let state = reducer(initialState, {type: 'INC'});
+//  state = reducer(state, {type: 'INC'});
+//  state = reducer(state, {type: 'INC'});
+//  state = reducer(state, {type: 'INC'});
+// console.log(state)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
