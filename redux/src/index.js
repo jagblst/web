f6e09831fdb7a5e099b3961590887e864a3c4f27
Jaga <jagblst@gmail.com>
2022-12-02 15:25:14 +0200

    Добавим React в проект

--- redux/src/index.js
+++ redux/src/index.js
@@ -1,34 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createStore, bindActionCreators } from 'redux';
+import { createStore } from 'redux';
 import reducer from './reducer';
-import * as actions from './actions';
+import {Provider} from 'react-redux';
 
-import Counter from './counter';
+import App from './components/App';
 
 const store = createStore(reducer);
 
-const {dispatch, subscribe, getState} = store;
-
-const {inc, dec, rnd} = bindActionCreators(actions, dispatch);
-
-const update = () => {
-  const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
   root.render(
     <React.StrictMode>
-      <Counter
-        counter={getState().value}
-        inc={inc}
-        dec={dec}
-        rnd={() => {
-          const value = Math.floor(Math.random() * 10);
-          rnd(value);
-        }}
-      />
+      <Provider store={store}>
+        <App/>
+      </Provider>
     </React.StrictMode>
-  );
-};
-update();
-subscribe(update);
-
-
+);
