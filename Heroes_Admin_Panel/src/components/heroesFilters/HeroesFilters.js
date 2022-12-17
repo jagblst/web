550afe3ffb19c23a20bafdc522f4e29f45906b31
Jaga <jagblst@gmail.com>
2022-12-17 15:16:36 +0200

    Redux Toolkit: createAsyncThunk()
    Созданы filters АС при помощи createAsyncThunk

--- Heroes_Admin_Panel/src/components/heroesFilters/HeroesFilters.js
+++ Heroes_Admin_Panel/src/components/heroesFilters/HeroesFilters.js
@@ -3,15 +3,8 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import classNames from 'classnames';
 
-import { fetchFilters } from '../../actions';
-import { filtersChanged } from './filtersSlice';
-
+import { filtersChanged, fetchFilters } from './filtersSlice';
 import Spinner from '../spinner/Spinner';
-
-// Задача для этого компонента:
-// Фильтры должны формироваться на основании загруженных данных
-// Фильтры должны отображать только нужных героев при выборе
-// Активный фильтр имеет класс active
 
 const HeroesFilters = () => {
 
@@ -19,7 +12,6 @@ const HeroesFilters = () => {
     const dispatch = useDispatch();
     const {request} = useHttp();
 
-    // Запрос на сервер для получения фильтров и последовательной смены состояния
     useEffect(() => {
         dispatch(fetchFilters(request));
         // eslint-disable-next-line
@@ -36,10 +28,8 @@ const HeroesFilters = () => {
             return <h5 className="text-center mt-5">Фильтры не найдены</h5>
         }
 
-        // Данные в json-файле я расширил классами и текстом
         return arr.map(({name, className, label}) => {
 
-            // Используем библиотеку classnames и формируем классы динамически
             const btnClass = classNames('btn', className, {
                 'active': name === activeFilter
             });
