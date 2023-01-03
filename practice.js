3c20c424353bbb1a920d9d5cd56edeef170f9018
Jaga <jagblst@gmail.com>
2023-01-03 15:54:43 +0200

    8 kyu Training JS #11: loop statement --break,continue
    codewars

--- practice.js
+++ practice.js
@@ -110,6 +110,21 @@ console.log(some(someOfThis, (elem) => elem === "JavaS"));
 
 ==========================================CODEWARS==========================================
 
+8 kyu Training JS #11: loop statement --break,continue
+
+function grabDoll(dolls){
+  var bag=[];
+  for (var i = 0; i < dolls.length; i++){
+    if (dolls[i] == "Hello Kitty" || dolls[i] == "Barbie doll")
+    { bag.push(dolls[i]) }
+     else continue;
+   if (bag.length == 3) {
+     break;
+    }
+  } 
+  return bag;
+}
+
 8 kyu Freudian translator
 
 function toFreud(string) {
