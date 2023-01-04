8b0603659ca836055f81fecb945334a88f9b4bb8
Jaga <jagblst@gmail.com>
2023-01-04 15:56:16 +0200

    Redux Toolkit: createEntityAdapter()
    Созданы адаптеры с помощью createEntityAdapter (HeroesList)

--- Heroes_Admin_Panel/src/components/heroesList/HeroesList.js
+++ Heroes_Admin_Panel/src/components/heroesList/HeroesList.js
@@ -4,22 +4,17 @@ import { useDispatch, useSelector } from 'react-redux';
 import { CSSTransition, TransitionGroup} from 'react-transition-group';
 import { createSelector } from '@reduxjs/toolkit'; 
 
-import { heroDeleted, fetchHeroes } from './heroesSlice';
+import { heroDeleted, fetchHeroes, selectAll } from './heroesSlice';
 import HeroesListItem from "../heroesListItem/HeroesListItem";
 import Spinner from '../spinner/Spinner';
 
 import './heroesList.scss';
 
-// Задача для этого компонента:
-// При клике на "крестик" идет удаление персонажа из общего состояния
-// Усложненная задача:
-// Удаление идет и с json файла при помощи метода DELETE
-
 const HeroesList = () => {
 
     const filteredHeroesSelector = createSelector(
         (state) => state.filters.activeFilter,
-        (state) => state.heroes.heroes,
+        selectAll,
         (filter, heroes) => {
             if (filter === 'all') {
                 return heroes;
--- Heroes_Admin_Panel/src/components/heroesList/heroesSlice.js
+++ Heroes_Admin_Panel/src/components/heroesList/heroesSlice.js
@@ -1,10 +1,16 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk, createEntityAdapter } from "@reduxjs/toolkit";
 import {useHttp} from '../../hooks/http.hook';
 
-const initialState = {
-    heroes: [],
-    heroesLoadingStatus: 'idle', 
-}
+const heroesAdapter = createEntityAdapter();
+
+// const initialState = {
+//     heroes: [],
+//     heroesLoadingStatus: 'idle', 
+// }
+
+const initialState = heroesAdapter.getInitialState({
+    heroesLoadingStatus: 'idle'
+});
 
 export const fetchHeroes = createAsyncThunk(
     'heroes/fetchHeroes',
@@ -19,10 +25,10 @@ const heroesSlice = createSlice({
     initialState,
     reducers: {
         heroCreated: (state, action) => {
-                    state.heroes.push(action.payload);
+                    heroesAdapter.addOne(state, action.payload);
                 },
         heroDeleted: (state, action) => {
-                    state.heroes = state.heroes.filter(item => item.id !== action.payload);
+                    heroesAdapter.removeOne(state, action.payload);
                 } 
     },
     extraReducers: (builder) => {
@@ -30,9 +36,11 @@ const heroesSlice = createSlice({
             .addCase(fetchHeroes.pending, state => {state.heroesLoadingStatus = 'loading'})
             .addCase(fetchHeroes.fulfilled, (state, action) => {
                 state.heroesLoadingStatus = 'idle';
-                state.heroes = action.payload;})
+                heroesAdapter.setAll(state, action.payload);
+            })
             .addCase(fetchHeroes.rejected, state => {
-                state.heroesLoadingStatus = 'error';})
+                state.heroesLoadingStatus = 'error';
+            })
             .addDefaultCase(() => {})
     }
 });
@@ -40,6 +48,9 @@ const heroesSlice = createSlice({
 const {actions, reducer} = heroesSlice;
 
 export default reducer;
+
+export const {selectAll} = heroesAdapter.getSelectors(state => state.heroes);
+
 export const {
     heroesFetching,
     heroesFetched,
