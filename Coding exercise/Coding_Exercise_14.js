b25397aa35091ea3c7d3d32be0419f9d972f1b17
Jaga <jagblst@gmail.com>
2023-05-22 10:55:30 +0300

    Coding Exercise 14
    Продвинутая задача на работу с объектами и массивами
    sortStudentsByGroups

---
+++ Coding exercise/Coding_Exercise_14.js
@@ -0,0 +1,61 @@
+// Продвинутая задача на работу с объектами и массивами
+
+// У вас есть список учеников, которые хотят поиграть в игру:
+
+// const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
+// Но команд может быть только 3 по 3 человека. Напишите функцию sortStudentsByGroups, которая принимает в себя массив строк.
+
+// Внутри она сначала сортирует имена по алфавиту. Затем распределяет учеников по 3 человека в 3 группы по алфавитному порядку. Эти группы должны быть массивами. Как итог, функция возвращает новый массив с тремя командами и строкой как 4й элемент.
+
+// Пример:
+
+// sortStudentsByGroups(students)  =>
+
+// [
+//   [ 'Andrew', 'Ann', 'Bernard' ],
+//   [ 'Cris', 'Josh', 'Mark' ],
+//   [ 'Peter', 'Sam', 'Sandra' ],
+//   'Оставшиеся студенты: Takesi'
+// ]
+// Если убрать одно студента из списка, то результат будет:
+
+// [
+//   [ 'Andrew', 'Ann', 'Bernard' ],
+//   [ 'Cris', 'Josh', 'Mark' ],
+//   [ 'Peter', 'Sam', 'Sandra' ],
+//   'Оставшиеся студенты: -'
+// ]
+// А если добавить одного, то:
+
+// [
+//   [ 'Andrew', 'Ann', 'Bernard' ],
+//   [ 'Cris', 'Josh', 'Mark' ],
+//   [ 'Peter', 'Sam', 'Sandra' ],
+//   'Оставшиеся студенты: Takesi, Somebody'
+// ]
+// То есть, меняется содержимое строки. Все оставшиеся ученики попадают туда.
+
+const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
+
+function sortStudentsByGroups(arr) {
+    let sorted = arr.sort()
+    let t1 = [];
+    let t2 = [];
+    let t3 = [];
+    let rest = [];
+    for (let i = 0; i < sorted.length; i++){
+        if (i < 3) {
+            t1.push(sorted[i]);
+        }
+        else if (i < 6) {
+            t2.push(sorted[i]);
+        }
+        else if (i < 9) {
+            t3.push(sorted[i]);
+        }
+        else {
+            rest.push(sorted[i]);
+        }
+    }
+    return [t1, t2, t3, `Оставшиеся студенты: ${rest.length === 0 ? '-' : rest.join(', ')}`]
+}
