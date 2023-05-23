1fe6b95df489d917f250e246336c8e3d8eaa5d68
Jaga <jagblst@gmail.com>
2023-05-23 18:10:00 +0300

    Coding Exercise 20
    Задания на работу с методами массивов
    showGoodFilms
    showListOfFilms
    setFilmsIds
    checkFilms

---
+++ Coding exercise/Coding_Exercise_20.js
@@ -0,0 +1,60 @@
+// Задания на работу с методами массивов
+
+// 1) У вас есть список фильмов с рейтингом в виде массива объектов. Напишите функцию showGoodFilms, которая будет принимать этот массив, а возвращать будет массив объектов только с теми фильмами, у которых рейтинг больше или равен 8.
+
+const films = [
+    {
+        name: 'Titanic',
+        rating: 9
+    },
+    {
+        name: 'Die hard 5',
+        rating: 5
+    },
+    {
+        name: 'Matrix',
+        rating: 8
+    },
+    {
+        name: 'Some bad film',
+        rating: 4
+    }
+];
+
+function showGoodFilms(arr) {
+    return arr.filter(e => e.rating >= 8);
+}
+
+// 2) Напишите функцию showListOfFilms, которая будет принимать этот же массив, а возвращать будет строку, которая содержит названия фильмов через запятую.
+
+// Пример:
+
+// showListOfFilms(films) => "Titanic, Die hard 5, Matrix, Some bad film";
+
+function showListOfFilms(arr) {
+    let res = '';
+    for (let i = 0; i < arr.length; i++){
+        res += `${arr[i].name}, `;
+    }
+    return res.slice(0, -2);
+}
+
+// 3) Напишите функцию setFilmsIds, которая будет принимать этот же массив, а возвращать будет такой же массив с фильмами, но у каждого фильма будет новое поле id. Значение этого поля установите по нумерации фильма.
+
+// Пример:
+
+// setFilmsIds(films)  => [   { name: 'Titanic', rating: 9, id: 0 },   { name: 'Die hard 5', rating: 5, id: 1 },   { name: 'Matrix', rating: 8, id: 2 },   { name: 'Some bad film', rating: 4, id: 3 } ]
+
+function setFilmsIds(arr) {
+    return arr.map((e, i) => {e.id = i; return e;})
+}
+
+// 4) Запишите результат предыдущей функции в переменную tranformedArray. Напишите функцию checkFilms, которая будет проверять, что в каждом из фильмов есть поле id. Если это так - функция возвращает true. Очевидно, что сейчас условие должно выполняться, если мы передаем checkFilms(tranformedArray); :)
+
+// P.S. Вот тут вы столкнетесь с интересным моментом, который я хочу, чтобы вы запомнили. Внимательно проследите за тем, что происходит внутри коллбэка и что будет проверяться.
+
+const tranformedArray = setFilmsIds(films);
+
+function checkFilms(arr) {
+    return arr.every(e => e.id || e.id == 0)
+}
